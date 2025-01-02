2ccd582e38a3e61f241a360660506ea9cbeb4278
Eelis Keränen <keraseneelis@gmail.com>
2025-01-02 19:23:47 +0200

    Removed useless comments

--- source/main.py
+++ source/main.py
@@ -6,14 +6,13 @@ from tkinter import *
 from tkinter import simpledialog
 from functools import partial
 import array
-# Generator
+
 
 
 def generator():
     MAX_LEN = usergeneratedpwlen
 
-    # declare arrays of the character that we need in out password
-    # Represented as chars to enable easy string concatenation
+
     DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                          'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
@@ -28,24 +27,19 @@ def generator():
     SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
                '*', '(', ')', '<']
 
-    # combines all the character arrays above to form one array
+    
     COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
 
-    # randomly select at least one character from each character set above
+   
     rand_digit = random.choice(DIGITS)
     rand_upper = random.choice(UPCASE_CHARACTERS)
     rand_lower = random.choice(LOCASE_CHARACTERS)
     rand_symbol = random.choice(SYMBOLS)
 
-    # combine the character randomly selected above
-    # at this stage, the password contains only 4 characters but
-    # we want a 12-character password
+
     temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
 
-    # now that we are sure we have at least one character from each
-    # set of characters, we fill the rest of
-    # the password length by selecting randomly from the combined
-    # list of character above.
+
     for x in range(int(MAX_LEN) - 4):
         temp_pass = temp_pass + random.choice(COMBINED_LIST)
 
@@ -81,7 +75,6 @@ password TEXT NOT NULL
 );
 """)
 
-# Create POPUP
 
 
 def popUp(text):
